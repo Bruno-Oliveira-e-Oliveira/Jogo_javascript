a64ad69e6602baf7859200ef8bdf8dac90c0de13
Bruno Oliveira <31875213+Bruno-Oliveira-e-Oliveira@users.noreply.github.com>
2020-05-23 13:11:57 -0300

    Criando a base do html, o modal que inicia o jogo, e preparando o canvas.

---
+++ js/game.js
@@ -0,0 +1,16 @@
+function startGame() {
+
+    const tela = document.getElementById('startGame');
+    tela.style.display = 'none';
+
+
+    const game = document.getElementById('game');
+    var context = game.getContext('2d');
+
+    
+
+}
+
+
+
+
