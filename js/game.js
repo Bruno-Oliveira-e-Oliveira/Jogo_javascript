7b0559440ca580caaf8094f016ec2e6b7299ebc8
Bruno Oliveira <31875213+Bruno-Oliveira-e-Oliveira@users.noreply.github.com>
2020-05-27 22:26:35 -0300

    Criando os inimigos, refatorando o objetos de player e a drawPlayer para drawObject, e desenhando os inimigos na tela sem permitir que ultrapassem a area do canvas.

--- js/game.js
+++ js/game.js
@@ -22,7 +22,6 @@ document.addEventListener('keydown', event => {
     }
 
     session.setItem('player', JSON.stringify(player)); 
-    session.setItem('game', JSON.stringify(game)); 
 });
 
 
@@ -34,19 +33,25 @@ function startGame() {
     const player = {
         x: 0,
         y: 0,
-        width: 5,
-        height: 5,
+        width: 4,
+        height: 4,
         move: 2,
-        points: 0
+        points: 0,
+        hp:2,
+        color: "#34F00A"
     }
     const game = {
         width: canvas.width,
         height: canvas.height,
-        max_points: 10
+        max_points: 10,
+        max_enemies: 5
     }
-    const enemies = {
-        quantity: 0
-    }
+    var enemies = []
+
+    player.x = (game.width/2) - (player.width/2)
+    player.y = (game.height/2) - (player.height/2)
+
+    enemies = createEnemies(game, enemies, player);
     
     startWindow.style.display = 'none';
     
@@ -58,6 +63,7 @@ function startGame() {
     window.requestAnimationFrame(runGame);
 }
 
+
 function runGame() {
     const canvas = document.getElementById('game');
     const context = canvas.getContext('2d');
@@ -68,23 +74,49 @@ function runGame() {
     var game = JSON.parse(session.getItem("game"));
     
     context.clearRect(0, 0, game.width, game.height);
-
-    drawPlayer(context, player);
-    createEnemies(enemies, player);
     
+    drawObject(context, player);
+
+    for (const key in enemies) {
+        drawObject(context, enemies[key]);    
+    }
+    
+    session.setItem('enemies', JSON.stringify(enemies));
+
     if (run) {
         window.requestAnimationFrame(runGame);
     }
 }
 
-function drawPlayer(context, player) {
-    context.fillStyle = "#34F00A";     
-    context.fillRect(player.x, player.y, player.width, player.height);
+
+function drawObject(context, object) {
+    context.fillStyle = object.color;     
+    context.fillRect(object.x, object.y, object.width, object.height);
     context.save() 
 }
 
-function createEnemies(enemies, player) {
-    //To do
+
+function createEnemies(game, enemies, player) {
+    for (let i = 1; i <= game.max_enemies; i++) {
+        const enemy = {
+            x: 0, 
+            y: 0,
+            width: 4,
+            height: 4,
+            move: 1,
+            hp:1,
+            color: "#FA1C0F"
+        }
+
+        let x = Math.floor(Math.random() * game.width);
+        let y = Math.floor(Math.random() * game.height);
+        enemy.x = ((x + enemy.width) > game.width) ?  (x - enemy.width) : x;
+        enemy.y = ((y + enemy.height) > game.height) ? (y - enemy.height) : y;
+
+        enemies.push(enemy)
+    }
+
+    return enemies
 }
 
 
